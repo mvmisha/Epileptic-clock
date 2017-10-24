e912ffd2fd1228f1985a827b094ee1bb4bdb06bf
Misha Mykhaylov <mishamykhaylov13@gmail.com>
2017-10-24 23:28:09 +0200

    shaking fixed

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function reloj(){
     horas = (horas < 10)?"0"+horas:horas;
     mins = (mins < 10)?"0"+mins:mins;
     segs = (segs<10)?"0"+segs:segs;
-    milsgs = fecha.getMilliseconds();//(milsgs<100)?"00"+milsgs:milsgs;
+    milsgs = fecha.getMilliseconds();
     if (milsgs<10){
           milsgs='00'+milsgs;     
     } else if (milsgs<100){
